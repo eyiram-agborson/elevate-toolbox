d94b1ee2df139f7a5933213f8500b6ea364212dc
eyiram-agborson <eyiram.agborson@etranzact.com.gh>
2026-08-27 10:42:39 +0000

    Add validation modal and other minor fixes

--- src/app/hr/validation/validation.ts
+++ src/app/hr/validation/validation.ts
@@ -6,4 +6,35 @@ import { Component } from '@angular/core';
   templateUrl: './validation.html',
   styleUrl: './validation.css',
 })
-export class Validation {}
+export class Validation {
+showValidateModal = false;
+selectedValidationScore: number | null = null;
+
+validationScores = [1, 2, 3, 4, 5];
+
+openValidateModal(): void {
+  this.selectedValidationScore = null;
+  this.showValidateModal = true;
+}
+
+closeValidateModal(): void {
+  this.showValidateModal = false;
+  this.selectedValidationScore = null;
+}
+
+selectValidationScore(score: number): void {
+  this.selectedValidationScore = score;
+}
+
+saveValidationScore(): void {
+  if (this.selectedValidationScore === null) {
+    return;
+  }
+
+  console.log('Validation score:', this.selectedValidationScore);
+
+  this.closeValidateModal();
+}
+
+
+}
