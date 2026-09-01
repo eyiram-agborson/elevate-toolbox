db05ecc1aa9ead6cecdaeb98fefcb06dee7169eb
eyiram-agborson <eyiram.agborson@etranzact.com.gh>
2026-09-01 08:46:33 +0000

    Hr fixes

--- src/app/hr/kpi-templates/kpi-templates.ts
+++ src/app/hr/kpi-templates/kpi-templates.ts
@@ -1,35 +1,82 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-kpi-templates',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './kpi-templates.html',
   styleUrl: './kpi-templates.css',
 })
 export class KpiTemplates {
 
   isEditKpi = false;
+  showAddTemplateModal = false;
 
-openEditKpi(): void {
-  this.isEditKpi= true;
-}
+  kpiForm: FormGroup;
 
-closeEditKpi(): void {
-  this.isEditKpi= false;
-}
+  constructor(private fb: FormBuilder) {
+    this.kpiForm = this.fb.group({
+      department: ['Client-side Engineering'],
+      jobRole: ['Software Engineer'],
+      kpis: this.fb.array([
+        this.createKpi(
+          'Application Development & Delivery',
+          60,
+          'Deliver assigned work within agreed sprint timelines.'
+        ),
+        this.createKpi(
+          'Operational Efficiency & Code Quality',
+          40,
+          'Maintain code quality and review standards.'
+        )
+      ])
+    });
+  }
 
-saveTemplate(): void {
-  this.isEditKpi= false;
-}
+  createKpi(name = '', weight = 0, description = ''): FormGroup {
+    return this.fb.group({
+      name: [name],
+      weight: [weight],
+      description: [description]
+    });
+  }
 
-showAddTemplateModal = false;
+  get kpis(): FormArray {
+    return this.kpiForm.get('kpis') as FormArray;
+  }
 
-openAddTemplate(): void {
-  this.showAddTemplateModal = true;
-}
+  addKpi(): void {
+    this.kpis.push(this.createKpi());
+  }
 
-closeAddTemplate(): void {
-  this.showAddTemplateModal = false;
-}
+  removeKpi(index: number): void {
+    this.kpis.removeAt(index);
+  }
 
+  get totalWeight(): number {
+    return this.kpis.controls.reduce((total, kpi) => {
+      return total + Number(kpi.get('weight')?.value || 0);
+    }, 0);
+  }
+
+  openEditKpi(): void {
+    this.isEditKpi = true;
+  }
+
+  closeEditKpi(): void {
+    this.isEditKpi = false;
+  }
+
+  saveTemplate(): void {
+    console.log(this.kpiForm.value);
+    this.isEditKpi = false;
+  }
+
+  openAddTemplate(): void {
+    this.showAddTemplateModal = true;
+  }
+
+  closeAddTemplate(): void {
+    this.showAddTemplateModal = false;
+  }
 }
