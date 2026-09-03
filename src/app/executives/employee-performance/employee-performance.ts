e840f54871cd6da3dc54e71cf761d974696b4e38
eyiram-agborson <eyiram.agborson@etranzact.com.gh>
2026-09-03 10:54:41 +0000

    Employee Performance page done

--- src/app/executives/employee-performance/employee-performance.ts
+++ src/app/executives/employee-performance/employee-performance.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { NzCollapseModule } from "ng-zorro-antd/collapse";
 
 @Component({
   selector: 'app-employee-performance',
-  imports: [],
+  imports: [NzCollapseModule],
   templateUrl: './employee-performance.html',
   styleUrl: './employee-performance.css',
 })
@@ -11,7 +12,26 @@ export class EmployeePerformance {
 
    constructor(private router: Router){}
 
-   goToDashboad(){
+
+   goToDashboard(){
     this.router.navigate(["/dashboard"])
   }
+
+  goToManagerPerformance(){
+    this.router.navigate(["executives/manager-performance"])
+  }
+
+  showEmployeeModal = false;
+
+  isStatusNotSubmitted = true;
+
+
+openEmployeeDetails(employeeName: string): void {
+  // later this will select the actual employee from your API data
+  this.showEmployeeModal = true;
+}
+
+closeEmployeeDetails(): void {
+  this.showEmployeeModal = false;
+}
 }
